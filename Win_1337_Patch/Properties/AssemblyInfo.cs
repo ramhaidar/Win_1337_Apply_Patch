6867517b507e1f7d76451a6a7bd8b75d693010b0
ramhaidar <49301219+ramhaidar@users.noreply.github.com>
2026-02-04 10:19:55 +0700

    build(assembly): update assembly metadata
    - Update assembly metadata file with version and product information
    - Target: assembly configuration

--- Win_1337_Patch/Properties/AssemblyInfo.cs
+++ Win_1337_Patch/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // È possibile specificare tutti i valori oppure impostare valori predefiniti per i numeri relativi alla revisione e alla build
 // usando l'asterisco '*' come illustrato di seguito:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.0.0")]
-[assembly: AssemblyFileVersion("2.1.0.0")]
+[assembly: AssemblyVersion("2.2.0.0")]
+[assembly: AssemblyFileVersion("2.2.0.0")]
